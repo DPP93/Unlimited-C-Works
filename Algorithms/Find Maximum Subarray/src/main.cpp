580238c09d47a8d160c25485dac38a06c10c3683
Daniel <dpeczek93@gmail.com>
2016-09-01 19:34:51 +0200

    Prepare list of Todos for Find Maximum Subarray Algorithm

---
+++ Algorithms/Find Maximum Subarray/src/main.cpp
@@ -0,0 +1,21 @@
+//============================================================================
+// Name        : main.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Divide and Conquer algorithm, where we're finding the part
+// of array, which give us maximum value, for example, we've got an array
+// of actions of some company, one day is one value. We would like to know
+// when buy cheap and sell good (don't know how to say it :P)
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+//TODO: What I need
+// - Function which find max crossing subarray - subarray which have middle element from array
+// - Main function which start recrusive method which do all what i need
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
