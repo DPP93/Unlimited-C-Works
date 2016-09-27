8684e72936985045fcf7eb11afe04475c8505cd0
dpeczek <dpeczek93@gmail.com>
2016-09-27 17:57:16 +0200

    Introduction to OpenCV with loading and modyfiing images

---
+++ OpenCV/DisplayImage/src/main.cpp
@@ -0,0 +1,22 @@
+#include <cstdio>
+#include <core.hpp>
+#include <imgproc.hpp>
+#include <highgui.hpp>
+
+int main( int argc, char** argv )
+{
+  cv::Mat image;
+  image = cv::imread( argv[1], cv::IMREAD_COLOR );
+
+  if( argc != 2 || !image.data )
+    {
+      printf( "No image data \n" );
+      return -1;
+    }
+  cv::Mat image2(image, cv::Rect(100, 100, 200, 200));
+  cv::namedWindow( "Display Image", cv::WINDOW_AUTOSIZE );
+  cv::imshow( "Display Image", image2 );
+  cv::waitKey(0);
+
+  return 0;
+}
