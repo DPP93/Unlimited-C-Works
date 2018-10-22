80cb5dc3a936e49ce4d25458a30a4245638afaff
dpp <dpeczek93@gmail.com>
2018-10-22 19:52:31 +0200

    Some CMake initial beings

--- CMakeTutorial/Let's Start/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-int main(int argc, char* argv[]) {
-	cout << "Hello Sir, have a nice day :D" << endl;
-	return 0;
-}
---
+++ CMakeTutorial/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+
+using namespace std;
+
+int main () {
+
+  double x;
+  cout << "Give me number to sqrt ";
+  cin >> x;
+
+  cout << "Result is " << sqrt(x) << endl;
+  return 0;
+}
